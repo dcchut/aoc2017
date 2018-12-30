d9335054691c786b596708be3181890eed32283b
Robert Usher <266585+dcchut@users.noreply.github.com>
2018-12-29 17:35:35 -0800

    Added empty Day 16 solution, fixed up Day 10 solution

--- Day10.py
+++ Day10.py
@@ -14,43 +14,43 @@ def wrap_reverse(li, idx, length):
 
         return sublist[len(li)-idx:] + li[overflow:idx] + sublist[:len(li)-idx]
 
+if __name__ == '__main__':
+    numbers = list(range(256))
+    current_position = 0
+    skip_size = 0
+    lengths = get_numbers(load_input('puzzle_inputs/Day10.txt')[0])
 
-numbers = list(range(256))
-current_position = 0
-skip_size = 0
-lengths = get_numbers(load_input('puzzle_inputs/Day10.txt')[0])
-
-for length in lengths:
-    numbers = wrap_reverse(numbers, current_position % len(numbers), length)
-    current_position += length + skip_size
-    skip_size += 1
-
-print('Part 1:', numbers[0] * numbers[1])
-
-# PART 2
-lengths = [ord(c) for c in load_input('puzzle_inputs/Day10.txt')[0]]
-lengths.extend([17, 31, 73, 47, 23])
-
-current_position = 0
-skip_size = 0
-numbers = list(range(256))
-
-for round in range(64):
     for length in lengths:
         numbers = wrap_reverse(numbers, current_position % len(numbers), length)
         current_position += length + skip_size
         skip_size += 1
 
-# xor & hex stage
-hash = ''
+    print('Part 1:', numbers[0] * numbers[1])
 
-for k in range(256//16):
-    r = numbers[k*16]
-    for p in numbers[k*16+1:(k+1)*16]:
-        r = r ^ p
-    h = hex(r)[2:]
-    if len(h) == 1:
-        h = '0' + h
-    hash += h
+    # PART 2
+    lengths = [ord(c) for c in load_input('puzzle_inputs/Day10.txt')[0]]
+    lengths.extend([17, 31, 73, 47, 23])
 
-print('Part 2:', hash)
+    current_position = 0
+    skip_size = 0
+    numbers = list(range(256))
+
+    for round in range(64):
+        for length in lengths:
+            numbers = wrap_reverse(numbers, current_position % len(numbers), length)
+            current_position += length + skip_size
+            skip_size += 1
+
+    # xor & hex stage
+    hash = ''
+
+    for k in range(256//16):
+        r = numbers[k*16]
+        for p in numbers[k*16+1:(k+1)*16]:
+            r = r ^ p
+        h = hex(r)[2:]
+        if len(h) == 1:
+            h = '0' + h
+        hash += h
+
+    print('Part 2:', hash)
